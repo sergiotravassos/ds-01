c26ed6f993065a2224549928e24ef70c89eefa68
sergiotravassos <sergiotravassos@outlook.com>
2022-08-02 16:39:29 +0100

    find client by id, exception handling

--- src/main/java/com/sergiotravassos/clientscrud/resources/ClientResource.java
+++ src/main/java/com/sergiotravassos/clientscrud/resources/ClientResource.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sergiotravassos.clientscrud.entites.Client;
+import com.sergiotravassos.clientscrud.dto.ClientDTO;
 import com.sergiotravassos.clientscrud.services.ClientService;
 
 @RestController
@@ -19,8 +20,14 @@ public class ClientResource {
 	private ClientService clientService;
 	
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll(){
-		List<Client> list = clientService.findAll();
+	public ResponseEntity<List<ClientDTO>> findAll(){
+		List<ClientDTO> list = clientService.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+		ClientDTO dto = clientService.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 }
--- src/main/java/com/sergiotravassos/clientscrud/services/ClientService.java
+++ src/main/java/com/sergiotravassos/clientscrud/services/ClientService.java
@@ -1,13 +1,17 @@
 package com.sergiotravassos.clientscrud.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sergiotravassos.clientscrud.dto.ClientDTO;
 import com.sergiotravassos.clientscrud.entites.Client;
 import com.sergiotravassos.clientscrud.repositories.ClientRepository;
+import com.sergiotravassos.clientscrud.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -16,7 +20,15 @@ public class ClientService {
 	private ClientRepository clientRepository;
 	
 	@Transactional(readOnly = true)
-	public List<Client> findAll(){
-		return clientRepository.findAll();
+	public List<ClientDTO> findAll(){
+		List<Client> list = clientRepository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = clientRepository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity nor found"));
+		return new ClientDTO(entity);
 	}
 }
